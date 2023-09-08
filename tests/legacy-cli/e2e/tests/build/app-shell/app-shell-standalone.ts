e2a76ced439d2ac15db5a7cba39bd9bb7d7eb973
Alan Agius <alanagius@google.com>
2023-09-08 09:47:09 +0000

    test: update E2E tests setup to work with application builder
    This commit updates the E2E tests setup to account for the default builder change from `@angular-devkit/build-angular:browser` to `@angular-devkit/build-angular:application`.

--- tests/legacy-cli/e2e/tests/build/app-shell/app-shell-standalone.ts
+++ tests/legacy-cli/e2e/tests/build/app-shell/app-shell-standalone.ts
@@ -8,6 +8,28 @@ const snapshots = require('../../../ng-snapshot/package.json');
 
 export default async function () {
   await ng('generate', 'app', 'test-project-two', '--routing', '--standalone', '--skip-install');
+
+  // Setup webpack builder if esbuild is not requested on the commandline
+  const useWebpackBuilder = !getGlobalVariable('argv')['esbuild'];
+  if (useWebpackBuilder) {
+    await updateJsonFile('angular.json', (json) => {
+      const build = json['projects']['test-project-two']['architect']['build'];
+      build.builder = '@angular-devkit/build-angular:browser';
+      build.options = {
+        ...build.options,
+        main: build.options.browser,
+        browser: undefined,
+      };
+
+      build.configurations.development = {
+        ...build.configurations.development,
+        vendorChunk: true,
+        namedChunks: true,
+        buildOptimizer: false,
+      };
+    });
+  }
+
   await ng('generate', 'app-shell', '--project', 'test-project-two');
 
   const isSnapshotBuild = getGlobalVariable('argv')['ng-snapshots'];
@@ -30,9 +52,13 @@ export default async function () {
     }
   }
 
-  await ng('run', 'test-project-two:app-shell:development');
-  await expectFileToMatch('dist/test-project-two/browser/index.html', 'app-shell works!');
-
-  await ng('run', 'test-project-two:app-shell');
-  await expectFileToMatch('dist/test-project-two/browser/index.html', 'app-shell works!');
+  if (useWebpackBuilder) {
+    await ng('run', 'test-project-two:app-shell:development');
+    await expectFileToMatch('dist/test-project-two/browser/index.html', 'app-shell works!');
+    await ng('run', 'test-project-two:app-shell');
+    await expectFileToMatch('dist/test-project-two/browser/index.html', 'app-shell works!');
+  } else {
+    await ng('build', 'test-project-two');
+    await expectFileToMatch('dist/test-project-two/index.html', 'app-shell works!');
+  }
 }
--- tests/legacy-cli/e2e/tests/build/styles/include-paths.ts
+++ tests/legacy-cli/e2e/tests/build/styles/include-paths.ts
@@ -1,12 +1,8 @@
-import { getGlobalVariable } from '../../../utils/env';
 import { writeMultipleFiles, expectFileToMatch, replaceInFile, createDir } from '../../../utils/fs';
 import { ng } from '../../../utils/process';
 import { updateJsonFile } from '../../../utils/project';
 
 export default async function () {
-  // esbuild currently only supports Sass
-  const esbuild = getGlobalVariable('argv')['esbuild'];
-
   await createDir('src/style-paths');
   await writeMultipleFiles({
     'src/style-paths/_variables.scss': '$primary-color: red;',
@@ -32,15 +28,15 @@ export default async function () {
   await replaceInFile(
     'src/app/app.component.ts',
     `'./app.component.css\'`,
-    `'./app.component.scss'` + (esbuild ? '' : `, './app.component.less'`),
+    `'./app.component.scss', './app.component.less'`,
   );
 
   await updateJsonFile('angular.json', (workspaceJson) => {
     const appArchitect = workspaceJson.projects['test-project'].architect;
-    appArchitect.build.options.styles = [{ input: 'src/styles.scss' }];
-    if (!esbuild) {
-      appArchitect.build.options.styles.push({ input: 'src/styles.less' });
-    }
+    appArchitect.build.options.styles = [
+      { input: 'src/styles.scss' },
+      { input: 'src/styles.less' },
+    ];
     appArchitect.build.options.stylePreprocessorOptions = {
       includePaths: ['src/style-paths'],
     };
