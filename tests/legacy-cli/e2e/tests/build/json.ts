6a205bbd8899cab8dd418f026048ed294581a18e
Alan Agius <alanagius@google.com>
2023-10-05 11:26:45 +0000

    test: update tests to account for changes in the build output structure
    This commit updates the tests to account for the new build structure.

--- tests/legacy-cli/e2e/tests/build/json.ts
+++ tests/legacy-cli/e2e/tests/build/json.ts
@@ -1,9 +1,15 @@
+import { getGlobalVariable } from '../../utils/env';
 import { expectFileToExist } from '../../utils/fs';
 import { expectGitToBeClean } from '../../utils/git';
 import { ng } from '../../utils/process';
 
 export default async function () {
   await ng('build', '--stats-json', '--configuration=development');
-  await expectFileToExist('./dist/test-project/stats.json');
+  if (getGlobalVariable('argv')['esbuild']) {
+    // esbuild uses an 8 character hash and a dash as separator
+    await expectFileToExist('./dist/test-project/stats.json');
+  } else {
+    await expectFileToExist('./dist/test-project/browser/stats.json');
+  }
   await expectGitToBeClean();
 }
